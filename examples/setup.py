bd93e8ef1b983321897042d73c478629e6ef20da
Floren Munteanu <fmunteanu@users.noreply.github.com>
2021-02-03 13:54:27 -0500

    Added code example (#62)

---
+++ examples/setup.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python
+
+from opencore.build import OpenCoreBuild
+
+
+if __name__ == '__main__':
+    kexts = [
+        {
+            'project': 'Lilu',
+            'repo': 'acidanthera',
+            'version': '1.5.1'
+        },
+        {
+            'project': 'NightShiftEnabler',
+            'repo': 'cdf',
+            'version': '1.1.0'
+        },
+        {
+            'project': 'WhateverGreen',
+            'repo': 'acidanthera',
+            'version': '1.4.7'
+        }
+    ]
+    build = OpenCoreBuild('Volumes/EFI', kexts)
+    build.write_tree()
+
+    settings = {
+        'DeviceProperties': {
+            'Add': {
+                'PciRoot(0x0)/Pci(0x3,0x0)/Pci(0x0,0x0)': {
+                    'agdpmod': build.unhexlify('70 69 6B 65 72 61 00'),
+                    'rebuild-device-tree': build.unhexlify('00'),
+                    'shikigva': build.unhexlify('50'),
+                    'unfairgva': build.unhexlify('01 00 00 00')
+                },
+                'PciRoot(0x0)/Pci(0x7,0x0)/Pci(0x0,0x0)/Pci(0x0,0x0)/Pci(0x0,0x0)': {
+                    'built-in': build.unhexlify('00')
+                },
+                'PciRoot(0x0)/Pci(0x7,0x0)/Pci(0x0,0x0)/Pci(0x8,0x0)/Pci(0x0,0x0)': {
+                    'built-in': build.unhexlify('00')
+                }
+            }
+        },
+        'Kernel': {
+            'Emulate': {
+                'Cpuid1Data': build.unhexlify('00 00 00 00 00 00 00 00 00 00 00 80 00 00 00 00'),
+                'Cpuid1Mask': build.unhexlify('00 00 00 00 00 00 00 00 00 00 00 80 00 00 00 00'),
+                'DummyPowerManagement': False
+            }
+        },
+        'Misc': {
+            'Boot': {
+                'PickerMode': 'External'
+            }
+        },
+        'NVRAM': {
+            'Add': {
+                '4D1EDE05-38C7-4A6A-9CC6-4BCCA8B38C14': {
+                    'DefaultBackgroundColor': build.unhexlify('00 00 00 00'),
+                    'UIScale': build.unhexlify('02')
+                }
+            },
+            'Delete': {
+                '4D1EDE05-38C7-4A6A-9CC6-4BCCA8B38C14': [
+                    'DefaultBackgroundColor',
+                    'UIScale'
+                ]
+            }
+        },
+        'PlatformInfo': {
+            'SMBIOS': {
+                'BIOSVersion': '9999.0.0.0.0',
+                'BoardProduct': 'Mac-7BA5B2D9E42DDD94'
+            },
+            'UpdateSMBIOS': True
+        }
+    }
+    build.write_plist(settings)
+    build.run_misc_tasks()

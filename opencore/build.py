8f685e4a0f5d9d7f1e5bfe45698730bd15517a17
Floren Munteanu <floren@axivo.com>
2021-12-09 10:21:23 -0500

    Added new release properties

--- opencore/build.py
+++ opencore/build.py
@@ -302,10 +302,12 @@ class OpenCoreBuild:
                     'GopPassThrough': 'Disabled',
                     'IgnoreTextInGraphics': False,
                     'ProvideConsoleGop': False,
+                    'ReconnectGraphicsOnConnect': False,
                     'ReconnectOnResChange': False,
                     'ReplaceTabWithSpace': False,
                     'Resolution': '',
                     'SanitiseClearScreen': False,
+                    'UIScale': -1,
                     'TextRenderer': 'BuiltinGraphics',
                     'UgaPassThrough': False
                 },
@@ -332,6 +334,7 @@ class OpenCoreBuild:
                 'Quirks': {
                     'ActivateHpetSupport': False,
                     'EnableVectorAcceleration': False,
+                    'EnableVmx': False,
                     'DisableSecurityPolicy': False,
                     'ExitBootServicesDelay': 0,
                     'ForceOcWriteFlash': False,

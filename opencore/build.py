ffbc5311bc5c52cb41233ef098a487d6127b5c49
Floren Munteanu <floren@axivo.com>
2022-02-07 14:51:37 -0500

    Updated versions

--- opencore/build.py
+++ opencore/build.py
@@ -355,7 +355,7 @@ class OpenCoreBuild:
                 'ReservedMemory': []
             }
         }
-        self.version = '0.7.7'
+        self.version = '0.7.8'
 
 
     def configure_kexts(self, kexts=[]):

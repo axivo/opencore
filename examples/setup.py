3f0313826b58110e62b334fc33050c9f6dca899e
Floren Munteanu <floren@axivo.com>
2022-03-08 19:44:41 -0500

    Added custom timeout

--- examples/setup.py
+++ examples/setup.py
@@ -52,7 +52,8 @@ if __name__ == '__main__':
                 'PollAppleHotKeys': True,
                 'PickerMode': 'External',
                 'PickerVariant': 'Default',
-                'ShowPicker': True
+                'ShowPicker': True,
+                'Timeout': 10
             },
             'Security': {
                 'AllowSetDefault': True,

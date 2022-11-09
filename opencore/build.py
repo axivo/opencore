6c98ddf47557907126be25dac653c4620805b5cf
Floren Munteanu <fmunteanu@users.noreply.github.com>
2022-11-09 16:00:28 -0500

    OC 0.8.6 upgrade (#134)
    * Update versions
    
    * Update properties

--- opencore/build.py
+++ opencore/build.py
@@ -274,7 +274,10 @@ class OpenCoreBuild:
                     'PointerPollMax': 0,
                     'PointerPollMin': 0,
                     'PointerSpeedDiv': 1,
-                    'PointerSpeedMul': 1
+                    'PointerSpeedMul': 1,
+                    'PointerDwellClickTimeout': 0,
+                    'PointerDwellDoubleClickTimeout': 0,
+                    'PointerDwellRadius': 0
                 },
                 'Audio': {
                     'AudioCodec': 0,
@@ -357,7 +360,7 @@ class OpenCoreBuild:
                 'ReservedMemory': []
             }
         }
-        self.version = '0.8.5'
+        self.version = '0.8.6'
 
 
     def configure_kexts(self, kexts=[]):
